245d749ef7d078d344db558d9657755c3220589f
BobH <bobh@zendee.cn>
2024-05-08 20:59:39 +0800

    update for ekf visualization

--- src/foxglove_info_publisher/foxglove_info_publisher/foxglove_info_publisher_exe.py
+++ src/foxglove_info_publisher/foxglove_info_publisher/foxglove_info_publisher_exe.py
@@ -20,12 +20,17 @@ class TrackPublisher(Node):
         super().__init__('track_publisher')
         self.path_deque = deque(maxlen=2000)
         self.rtk_deque = deque(maxlen=2000)
+        self.ekf_deque = deque(maxlen=2000)
         self.publisher_left = self.create_publisher(PolygonStamped, 'race_track_left', 10)
         self.publisher_right = self.create_publisher(PolygonStamped, 'race_track_right', 10)
         
         self.subscriber_rtk_observer = self.create_subscription(RtkObserver, "/bob_eye/rtk_observer", self.rtk_observer_callback, 1)
         self.publisher_rtk_path = self.create_publisher(Path, 'rtk_path', 10)
         self.publisher_rtk_pose = self.create_publisher(PoseStamped, 'rtk_pose', 10)
+        
+        self.subscriber_ekf_ins = self.create_subscription(VectornavIns, "/bob_eye/estimate_local", self.ekf_ins_callback, 1)
+        self.publisher_ekf_path = self.create_publisher(Path, 'ekf_path', 10)
+        self.publisher_ekf_pose = self.create_publisher(PoseStamped, 'ekf_pose', 10)
         
         self.subscriber_vn_ins = self.create_subscription(VectornavIns, "/a2rl/vn/ins", self.vn_ins_sub_callback, 1)
         self.publisher_ego_marker = self.create_publisher(Marker, 'ego_marker', 10)
@@ -36,6 +41,41 @@ class TrackPublisher(Node):
         self.boundary_left_file = os.path.join(boundary_path, "yas_full_left.csv")
         self.boundary_right_file = os.path.join(boundary_path, "yas_full_right.csv")
         self.timer = self.create_timer(5, self.publish_track)
+        
+    def ekf_ins_callback(self, msg):
+        self.ekf_deque.append((msg.position_enu_ins.x, msg.position_enu_ins.y))
+        q = self.quaternion_from_euler(msg.orientation_ypr.z, msg.orientation_ypr.y, msg.orientation_ypr.x)
+        path_data = [self.ekf_deque[i] for i in range(0, len(self.ekf_deque), 20)]
+        path_msg = Path()
+        path_msg.header = Header()
+        path_msg.header.stamp = self.get_clock().now().to_msg()
+        path_msg.header.frame_id = "map"
+        for (px, py) in path_data:
+            pose_stamped = PoseStamped()
+            pose_stamped.header.stamp = self.get_clock().now().to_msg()
+            pose_stamped.header.frame_id = "map"
+            pose_stamped.pose.position.x = float(px)
+            pose_stamped.pose.position.y = float(py)
+            pose_stamped.pose.position.z = 0.0
+            pose_stamped.pose.orientation.x = 0.0
+            pose_stamped.pose.orientation.y = 0.0
+            pose_stamped.pose.orientation.z = 0.0
+            pose_stamped.pose.orientation.w = 1.0
+            path_msg.poses.append(pose_stamped)
+        self.publisher_ekf_path.publish(path_msg)
+        pose_msg = PoseStamped()
+        pose_msg.header = Header()
+        pose_msg.header.stamp = self.get_clock().now().to_msg()
+        pose_msg.header.frame_id = "map"
+        pose_msg.pose.position.x = msg.position_enu_ins.x
+        pose_msg.pose.position.y = msg.position_enu_ins.y
+        pose_msg.pose.position.z = 0.0
+        pose_msg.pose.orientation.x = q[0]
+        pose_msg.pose.orientation.y = q[1]
+        pose_msg.pose.orientation.z = q[2]
+        pose_msg.pose.orientation.w = q[3]
+        self.publisher_ekf_pose.publish(pose_msg)
+    
     def rtk_observer_callback(self, msg):
         self.rtk_deque.append((msg.position_enu_rtk.x, msg.position_enu_rtk.y))
         q = self.quaternion_from_euler(msg.orientation_ypr_ins.z, msg.orientation_ypr_ins.y, msg.orientation_ypr_ins.x)
